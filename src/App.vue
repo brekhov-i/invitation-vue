70510defc93f77a1281d18383a38a8fb5a55b4fc
brekhov-i <brekhov-i@yandex.ru>
2025-07-31 22:45:28 +0300

    upcommit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template>
-  <template v-if="isValidAccess">
-    <main>
-      <MainSection />
-      <PlaceSection />
-      <PlanSection />
-      <DresscodeSection />
-      <MomentsSection />
-      <ContactsSection />
-      <FormSection />
-    </main>
-  </template>
-  <div v-else class="empty-page">
-    <div class="empty-content">
-      <h1>{{ ACCESS_CONFIG.MESSAGES.ACCESS_DENIED }}</h1>
-      <p>{{ ACCESS_CONFIG.MESSAGES.ACCESS_DENIED_DESCRIPTION }}</p>
-    </div>
-  </div>
+  <main>
+    <MainSection />
+    <PlaceSection />
+    <PlanSection />
+    <DresscodeSection />
+    <MomentsSection />
+    <ContactsSection />
+    <FormSection />
+  </main>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import {ref, onMounted} from 'vue'
 import MainSection from './widgets/MainSection.vue'
 import PlaceSection from './widgets/PlaceSection.vue'
 import PlanSection from './widgets/PlanSection.vue'
@@ -27,14 +19,14 @@ import DresscodeSection from './widgets/DresscodeSection.vue'
 import MomentsSection from './widgets/MomentsSection.vue'
 import ContactsSection from './widgets/ContactsSection.vue'
 import FormSection from './widgets/FormSection.vue'
-import { ACCESS_CONFIG, verifyPassword } from './shared/config/access'
+import {ACCESS_CONFIG, verifyPassword} from './shared/config/access'
 
 const isValidAccess = ref(false)
 
 onMounted(async () => {
   // Получаем пароль из URL (без символа #)
   const inputPassword = window.location.hash.slice(1)
-  
+
   if (inputPassword) {
     // Проверяем пароль с помощью bcrypt
     const isValid = await verifyPassword(inputPassword, ACCESS_CONFIG.SECRET_HASH)
@@ -65,13 +57,13 @@ main {
 .empty-content {
   text-align: center;
   padding: 2rem;
-  
+
   h1 {
     color: #333;
     margin-bottom: 1rem;
     font-size: 2rem;
   }
-  
+
   p {
     color: #666;
     font-size: 1.1rem;
