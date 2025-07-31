b82e680179f63021dce65fe7ac33352487011266
Илья <45898863+brekhov-i@users.noreply.github.com>
2025-07-31 07:40:55 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: [
+      'wedding-12-09.ru',
+      'www.wedding-12-09.ru',
+      'localhost'
+    ]
+  }
 })
